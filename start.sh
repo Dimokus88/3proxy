5bffbc6ca3377a15fa7d23995422e04ef1021734
Dimokus <Dimokus@mail.ru>
2022-09-05 11:14:16 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,3 +4,11 @@ git clone https://github.com/3proxy/3proxy.git
 cd 3proxy
 sudo make -f Makefile.Linux
 cp ./bin/3proxy /usr/bin
+
+cat /usr/bin/3proxy.cfg << EOF
+auth none
+log
+proxy -p8088
+EOF
+
+3proxy /usr/bin/3proxy.cfg
