1090f6af471aeef2a391414da7e0588d06ff7aaf
Dimokus <Dimokus@mail.ru>
2022-11-28 14:31:20 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,22 @@
 #!/bin bash
-apt-get install -y sudo make build-essential git tmate
+apt-get install -y sudo make build-essential git tmate runit
+runsvdir -P /etc/service &
+mkdir /root/tmate && mkdir /root/tmate/log
+cat > /root/tmate/run <<EOF 
+#!/bin/bash
+exec 2>&1
+exec tmate -F
+EOF
+cat > /root/tmate/log/run <<EOF 
+#!/bin/bash
+mkdir /var/log/tmate
+exec svlogd -tt /var/log/tmate
+EOF
+chmod +x /root/tmate/run
+chmod +x /root/tmate/log/run
+ln -s /root/tmate /etc/service
+
+
 git clone https://github.com/3proxy/3proxy.git
 cd 3proxy
 sudo make -f Makefile.Linux
